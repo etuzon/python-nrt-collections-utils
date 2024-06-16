0d4de464191b1e9d39dfd6e884ee6eac5c1eb98a
Eyal Tuzon <etuzon@cisco.com>
2024-06-17 00:05:25 +0300

    version 1.0.0

---
+++ tests/list_utils_test.py
@@ -0,0 +1,33 @@
+import pytest
+
+from nrt_collections_utils.list_utils import ListUtil
+from tests.list_utils_data import compare_lists_data
+
+
+@pytest.mark.parametrize('list_1, list_2, expected_result', compare_lists_data)
+def test_compare_lists(list_1, list_2, expected_result):
+    assert ListUtil.compare_lists(list_1, list_2) == expected_result
+
+
+def test_remove_none():
+    assert ListUtil.remove_none([1, None, 2, None, 3]) == [1, 2, 3]
+    assert ListUtil.remove_none([None, None, None]) == []
+    assert ListUtil.remove_none([1, 2, 3]) == [1, 2, 3]
+    assert ListUtil.remove_none([]) == []
+
+
+def test_remove_duplicates():
+    assert ListUtil.compare_lists(
+        ListUtil.remove_duplicates([1, 2, 3, 1, 2, 3]), [1, 2, 3])
+
+    assert ListUtil.compare_lists(ListUtil.remove_duplicates([1, 1, 1, 1, 1]), [1])
+
+    assert ListUtil.remove_duplicates([]) == []
+
+    assert ListUtil.compare_lists(ListUtil.remove_duplicates([1, 2, 3]), [1, 2, 3])
+
+    assert ListUtil.compare_lists(
+        ListUtil.remove_duplicates(['a', 'b', 'c', 'a', 'b', 'c']), ['a', 'b', 'c'])
+
+    assert ListUtil.compare_lists(
+        ListUtil.remove_duplicates(['a', None, 'b', None]), ['a', None, 'b'])
